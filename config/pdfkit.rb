4d04ad5a91bafbb9b1bcf3f73759e6732f573388
Vladislav Melanitskiy <vlad.melanitski@gmail.com>
2014-06-04 19:29:14 +0300

    Change config

--- config/pdfkit.rb
+++ config/pdfkit.rb
@@ -1,3 +1,13 @@
 PDFKit.configure do |config|
   config.wkhtmltopdf = '/usr/local/bin/wkhtmlpdf'
+  config.default_options = {
+      :disable_smart_shrinking => true,
+      :quiet => true,
+      :page_size => 'Letter',
+      :margin_top => '0',
+      :margin_right => '0',
+      :margin_bottom => '0',
+      :margin_left => '0',
+      :encoding => 'UTF-8'
+  }
 end
