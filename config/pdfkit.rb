871ca372df4ff268f6e7fd2797a96dca6a5833ff
Vladislav Melanitskiy <vlad.melanitski@gmail.com>
2014-06-04 19:36:50 +0300

    Remove useless options

--- config/pdfkit.rb
+++ config/pdfkit.rb
@@ -3,11 +3,6 @@ PDFKit.configure do |config|
   config.default_options = {
       :disable_smart_shrinking => true,
       :quiet => true,
-      :page_size => 'Letter',
-      :margin_top => '0',
-      :margin_right => '0',
-      :margin_bottom => '0',
-      :margin_left => '0',
       :encoding => 'UTF-8'
   }
 end
