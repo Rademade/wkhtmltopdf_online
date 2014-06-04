ae72b2dcce49cf844af634c60feba8689239e4b2
Vladislav Melanitskiy <vlad.melanitski@gmail.com>
2014-06-04 19:18:48 +0300

    Update configs

--- config/pdfkit.rb
+++ config/pdfkit.rb
@@ -1,7 +1,3 @@
 PDFKit.configure do |config|
   config.wkhtmltopdf = '/usr/local/bin/wkhtmlpdf'
-  config.default_options = {
-      :page_size => 'Legal',
-      :print_media_type => true
-  }
 end
